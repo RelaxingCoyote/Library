d765d9938eb9c31ee4c742da148e0a104c10e400
RelaxingCoyote <markoosha98@gmail.com>
2021-04-29 20:49:57 +0700

    yml change

---
+++ Library.Test/LibraryControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using Library.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Library.Test
+{
+    public class LibraryControllerTest
+    {
+        [Fact]
+        public void IndexViewResultNotNull()
+        {
+            // Arrange
+            LibraryController controller = new LibraryController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.NotNull(result);
+        }
+
+    }
+}
